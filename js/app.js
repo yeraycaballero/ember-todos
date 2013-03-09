f97f506a45ae3e0e690c7809bbb3d049931b47fc
yeraycaballero <yeraycaballero@gmail.com>
2013-03-09 19:08:29 +0000

    add changes

--- js/app.js
+++ js/app.js
@@ -9,19 +9,19 @@ App.Todo = Ember.Object.extend({
 
 App.Todos = [];
 
-App.ApplicationRoute = Ember.Route.extend({
-  
+App.ApplicationRoute = Ember.Route.extend({  
   model: function() {
     return App.Todos;
   }
 });
 
 App.ApplicationController = Ember.ArrayController.extend({
-  content : [],
+  content : [],  
 //  sortProperties: ['title'],
 //  sortAscending: true,
 
   createTodo : function(title) {
+    if (title == '') return;
     var todo = App.Todo.create({'title' : title});
     this.pushObject(todo);
   },
@@ -30,18 +30,33 @@ App.ApplicationController = Ember.ArrayController.extend({
     this.filterProperty('isDone', true).forEach(this.removeObject, this);
   },
 
-  allAreDone : function(key, value) {
-    console.log('all are done');
-  },
+  markAll : function(key, value) {
+   if (arguments.length === 2) {
+      this.setEach('isDone', value);
+      return value;
+    } else {
+      return ! this.get('isEmpty') && this.everyProperty('isDone', true); 
+    }
+  }.property('@each.isDone'),
 
   remaining : function() {
     return this.filterProperty('isDone', false).get('length');
   }.property('@each.isDone'),
 
+  noOneDone : function() {
+    return this.everyProperty('isDone', false);
+  }.property('@each.isDone'),
+
   isEmpty: function() {
     return this.get('length') === 0;
-  }.property('length')
+  }.property('length'),
 
+  markLabel : function() {    
+    console.log('markAll: ' + this.get('markAll') );
+
+    if (this.get('markAll') == true) return 'UnMark All'
+    return 'Mark All Done';
+  }.property('@each.isDone')
 
 });
 
@@ -53,6 +68,7 @@ App.ApplicationView = Ember.View.extend({
 
   didInsertElement : function() {
     this.textField.insertNewline = $.proxy(this._insertNewline, this);
+    this.textField.$().focus();
   },
 
   _insertNewline : function(event) {
